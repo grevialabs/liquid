6fa26b619e0e8aae40b625af5fd1979968b0238c
rusdi <rusdi.karsandi@gmail.com>
2019-02-05 20:48:08 +0700

    add Level,pic,reason,site mvc

---
+++ app/Models/LevelModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LevelModel extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'ms_level';
+
+    protected $fillables;
+
+
+}
---
+++ app/Models/ReasonModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ReasonModel extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'ms_reason';
+
+    protected $fillables;
+
+
+}
